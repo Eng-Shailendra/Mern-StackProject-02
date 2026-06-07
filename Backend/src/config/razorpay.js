d349463d71a20be00b03483208a2a9b07865ad16
shailendra sahu <shailendra2130@gmail.com>
2026-06-07 11:25:58 +0530

    added changes on razorpay

--- Backend/src/config/razorpay.js
+++ Backend/src/config/razorpay.js
@@ -1,7 +1,9 @@
 import Razorpay from "razorpay";
 import products from "razorpay/dist/types/products";
 
-const instance = new Razorpay({
+const paymentInstance = new Razorpay({
     key_id: process.env.RZP_API_KEY,
     key_secret: process.env.RZP_TEST_KEY,
 });
+
+export { paymentInstance }
--- Backend/src/controllers/payment-controller.js
+++ Backend/src/controllers/payment-controller.js
@@ -1,9 +1,56 @@
+import { verify } from "jsonwebtoken"
+import { paymentInstance } from "../config/razorpay.js"
 
 
 export const createdOrder = (req, res) => {
+    const option = {
+        amount: req.body.amount * 100,
+        currency: "INR",
+        recepit: crypto.randomBytes(10).toString("hex"),
+    }
+    try {
+        const order = paymentInstance.orders.create(option)
+        if (!order) {
+            return res.status(400).json({
+                success: false,
+                message: "Unabel to create your orrder payment"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "order suuccessfully created",
+            data: order
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            success: false,
+            message: "Internal error to on payment order"
+        })
+    }
 
 }
 
 export const paymentVerify = (req, res) => {
-
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
+        const generate_signature = crypto.createHmac("sha226", process.env.RZP_TEST_KEY).update(razorpay_order_id + "|" + razorpay_payment_id).digest("hex");
+        if (razorpay_signature !== generate_signature) {
+            return res.status(400).json({
+                success: false,
+                message: "Payment is not varified "
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "Payment is successfully verified "
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            success: false,
+            message: "Internal error to on payment verify"
+        })
+    }
 }
